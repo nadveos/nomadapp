0cc6ac25bf93c5bab254ae392f916b0d8577504d
je-martini <jesusmartini2017@gmail.com>
2023-05-10 09:11:22 -0400

    building the smart contract for  the transaction, making the function that confir that the cash provider is going to make the transaction and the release's funtion when the transacion is finish

--- smart-contracts/contracts/wallet_conecter/wallet_conecter.sol
+++ smart-contracts/contracts/wallet_conecter/wallet_conecter.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.2;
 
 contract purchase_confirmation {
 
-    uint public value1;
+    uint public money_to_change;
     address payable public nomada;
     address payable public cash_provider;
 
@@ -15,14 +15,35 @@ contract purchase_confirmation {
 
     state public contract_status;
 
+    /// Only the nomada can call this function
+    error just_nomada();
+
+    modifier only_nomada() {
+        if(msg.sender != nomada){
+            revert just_nomada();
+        }
+        _;
+    }
+
+
+    /// The function cannot be called at the current state.
+    error invalid_state();
+
+    modifier in_state( state state_){
+        if(contract_status != state_){
+            revert invalid_state();
+        }
+            _;
+    }
+
     function nomada_start_transaction(address payable cripto_seller, uint amount) payable public{
         nomada = payable(cripto_seller);
-        value1 = amount;
+        money_to_change = amount;
         contract_status = state.created;
     } 
 
     function transaction_agreement( address payable cripto_buyer, uint amount) payable external {
-        require(amount == value1, "you need to have the same colareal");
+        require(amount == money_to_change, "you need to have the same colareal");
         cash_provider = payable(cripto_buyer);
         
         contract_status = state.locked;
@@ -31,5 +52,11 @@ contract purchase_confirmation {
 
     function confirm_purchase() external payable {
         
+    }
+
+    function pay_cash_provider() external only_nomada() in_state(state.release){
+        contract_status = state.inactive;
+
+        cash_provider.transfer(2 * money_to_change);
     }
 }
---
+++ smart-contracts/contracts/wallet_conecter1/wallet_conecter1.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+
+contract purchase_confirmation {
+
+    uint private password;
+    uint public money_to_change;
+    address payable public nomada;
+    address payable public cash_provider;
+
+
+    enum state {
+        created, locked, release, inactive
+    }    
+
+    state public contract_status;
+
+    constructor(uint _password) payable {
+        nomada = payable(msg.sender);
+        password = _password;
+    }
+
+    /// The function cannot be called at the current state.
+    error invalid_state();
+
+    /// Only the cash provider can call this function
+    error just_cash_provider();
+
+    /// Only the nomada can call this function
+    error just_nomada();
+
+    modifier only_nomada() {
+        if(msg.sender != nomada){
+            revert just_nomada();
+        }
+        _;
+    }
+
+    modifier only_cash_provider() {
+        if(msg.sender != cash_provider){
+            revert just_cash_provider();
+        }
+        _;
+    }
+
+    modifier in_state( state state_){
+        if(contract_status != state_){
+            revert invalid_state();
+        }
+            _;
+    }
+
+    function cash_provider_confirm_transaction() external in_state(state.created) only_cash_provider payable {
+        
+        require(msg.value == money_to_change, "Please send the same amount that the nomada, to do change");
+        
+        cash_provider = payable(msg.sender);
+        
+        contract_status = state.locked;
+    }
+
+    function confirm_received(uint _password) external only_cash_provider() in_state(state.locked) {
+        // this funtion returns the deposit that 
+        // the cash_provider made to have a colateral
+        require(_password == password, "You need to enter the password that the nomada set up to release your funds"); 
+        cash_provider.transfer(2 * money_to_change);
+        contract_status = state.release;
+    }
+
+
+    function abort() external in_state(state.release) {
+        contract_status = state.inactive;
+
+        nomada.transfer(address(this).balance/2);
+        cash_provider.transfer(address(this).balance/2);
+    }
+
+
+}
